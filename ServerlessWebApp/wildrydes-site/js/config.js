543bef3d41bf3fc2be698a0e959481131183b8c8
webplusangels <nimweaver14@gmail.com>
2023-02-17 15:27:29 +0900

    Cognito Update

--- ServerlessWebApp/wildrydes-site/js/config.js
+++ ServerlessWebApp/wildrydes-site/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-northeast-2_oFEtAoEeL', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'ushfgahe0j7rud0u8avpm2inc', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
